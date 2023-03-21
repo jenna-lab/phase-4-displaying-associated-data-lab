55e8cf3bb1b802c201d67010f7200527644ee3dd
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-03-21 11:19:51 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
 class ItemsController < ApplicationController
+#GET /items
+def index
+    items = Item.all
+    render json: items.as_json(include: { user: { only: [:id, :username, :city] } }, only: [:id, :name, :description, :price])end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+# GET/users/:id
+def show
+    user =User.find(params[:id])
+    render json: user.as_json(include: { items: { only: [:id, :name, :description, :price] } }, only: [:id, :username, :city])end
 end
